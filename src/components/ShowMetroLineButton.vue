e38e1d0a4a01461468a4696586eec5320605bbec
koshi <koshi@digitaldolphins.jp>
2019-11-11 15:02:35 +0900

    指定の色で変更をできるようにした

---
+++ src/components/ShowMetroLineButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <button
+            class="btn_station"
+            v-if="LineVisible"
+            v-bind:style="StyleColor"
+            v-on:click="ColorChange(BeforeColor,ColorChoose)">
+            {{StationName}}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      StyleColor :"background:White"
+    }
+  },
+  props: {
+      BeforeColor: {
+        type:String
+      },
+      ColorChoose: {
+          type:String
+      },
+      LineName: {
+          type:String
+      },
+      LineVisible: {
+          type:Boolean
+      },
+      StationName: {
+        type: String
+      }
+
+  },
+  methods: {
+    ColorChange: function(BeforeColor,afterColor) {
+      this.StyleColor="background:" + afterColor;
+    }
+  }
+
+}
+
+</script>
